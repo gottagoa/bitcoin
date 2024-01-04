15457ae6428f4def65d7444208ec6d133db0b7cf
gottagoa <136464755+gottagoa@users.noreply.github.com>
2024-01-05 01:27:15 +0600

    channging urls

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import UserRegistrationView, UserUpdateView, ChangePasswordView
+from .views import UserRegistrationView, UserUpdateView, ChangePasswordView, GetUserDataVIew
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
 
 schema_view=get_schema_view(
     openapi.Info(
-        title='Hakathon',
+        title='Scrooge_McDuck',
         default_version='v1',
-        description='Bitcoin',
+        description='Scrooge_McDuck API',
         contact=openapi.Contact(email='palumna.f@gmail.com'),
         license=openapi.License(name='BSD License'),
         
@@ -20,5 +20,6 @@ urlpatterns=[
     path('docs/', schema_view.with_ui('swagger')),
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('update/', UserUpdateView.as_view(), name='update'),
-    path('change_password/', ChangePasswordView.as_view(), name='change_password')
+    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
+    path('get_user/', GetUserDataVIew.as_view()),
 ]
